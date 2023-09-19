d2cc3da9a3c3dfd55eb7817123ef9bed78406334
naksh1414 <115999604+naksh1414@users.noreply.github.com>
2023-09-19 22:57:21 +0530

    Add files via upload
    Env for About page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import About from './pages/aboutPage';
+
+function App() {
+  return (
+    <div className="App">
+      <About/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/aboutPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      {/* import all  the sections here from about-page  */}
+    </div>
+  )
+}
+
+export default About
